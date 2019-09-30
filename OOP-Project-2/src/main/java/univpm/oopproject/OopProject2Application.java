078d1b03702f0bee23bdd8b4e03b48cbfddc909d
SamuelePerticarari <samuele.perticarari@gmail.com>
2019-09-30 14:28:55 +0200

    Download TSV dataset.

--- OOP-Project-2/src/main/java/univpm/oopproject/OopProject2Application.java
+++ OOP-Project-2/src/main/java/univpm/oopproject/OopProject2Application.java
@@ -1,5 +1,7 @@
 package univpm.oopproject;
 
+import java.io.*;
+import java.net.URL;
 import java.util.Arrays;
 
 import org.springframework.boot.CommandLineRunner;
@@ -15,7 +17,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class OopProject2Application {
 
 	public static void main(String[] args) {
+		
+		
+		try
+		{
+			byte dataBuffer[] = new byte[1024];
+		    int bytesRead;
+		    
+		    System.out.println("SCARICO IL DATASET..");
+			
+			InputStream datasetStream = new URL( Configurations.FILE_URL ).openStream();
+			BufferedInputStream in = new BufferedInputStream( datasetStream  );
+			
+			FileOutputStream fileOutputStream = new FileOutputStream( Configurations.FILE_NAME );
+			
+		    while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+		        fileOutputStream.write(dataBuffer, 0, bytesRead);
+		    }
+		    
+		    datasetStream.close();
+		    fileOutputStream.close();
+		    in.close();
+			
+		    System.out.println("DATASET SCARICATO..");
+		} catch (IOException e)
+		{
+			System.err.println("Errore nel download del file...");
+			System.exit(-1);
+		}
+
 		SpringApplication.run(OopProject2Application.class, args);
+		
 	}
 
 
