16a64c2f9823b59899528cd0add3c0094ed9e1f6
Samuele Perticarari <samuele.perticarari@gmail.com>
2023-06-03 22:50:59 +0200

    Created Spring Boot app.

---
+++ src/test/java/com/samupert/univpm/eurostat/EurostatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samupert.univpm.eurostat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurostatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
