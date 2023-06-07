6c8f2906ee8c248cd5276199b356bb563123ec98
Samuele Perticarari <samuele.perticarari@gmail.com>
2023-06-07 23:58:49 +0200

    Created MonetaryPovertyService

---
+++ src/main/java/com/samupert/univpm/eurostat/monetary/poverty/MonetaryPovertyService.java
@@ -0,0 +1,17 @@
+package com.samupert.univpm.eurostat.monetary.poverty;
+
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class MonetaryPovertyService {
+
+    private final MonetaryPovertyRepository repository;
+
+    public Page<MonetaryPoverty> getAll(Pageable page) {
+        return this.repository.findAll(page);
+    }
+}
