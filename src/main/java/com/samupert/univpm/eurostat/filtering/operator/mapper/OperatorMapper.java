41e0360282b43249f4001051c44cf000221800ea
Samuele Perticarari <samuele.perticarari@gmail.com>
2023-06-08 15:48:35 +0200

    Created operator mapper for conditional and logical operators.

---
+++ src/main/java/com/samupert/univpm/eurostat/filtering/operator/mapper/OperatorMapper.java
@@ -0,0 +1,45 @@
+package com.samupert.univpm.eurostat.filtering.operator.mapper;
+
+import com.samupert.univpm.eurostat.common.mapper.DtoToEntityMapper;
+import com.samupert.univpm.eurostat.common.mapper.EntityToDtoMapper;
+import com.samupert.univpm.eurostat.filtering.exception.InvalidOperatorException;
+import com.samupert.univpm.eurostat.filtering.operator.Operator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OperatorMapper implements DtoToEntityMapper<String, Operator>, EntityToDtoMapper<String, Operator> {
+
+    private final LogicalOperatorMapper logicalOperatorMapper;
+    private final ConditionalOperatorMapper conditionalOperatorMapper;
+
+    public OperatorMapper(LogicalOperatorMapper logicalOperatorMapper,
+            ConditionalOperatorMapper conditionalOperatorMapper
+    ) {
+        this.logicalOperatorMapper = logicalOperatorMapper;
+        this.conditionalOperatorMapper = conditionalOperatorMapper;
+    }
+
+
+    @Override
+    public String getDto(Operator entity) {
+        return entity.toString();
+    }
+
+    @Override
+    public Operator getEntity(String operatorValue) throws InvalidOperatorException {
+
+        try{
+            return this.logicalOperatorMapper.getEntity(operatorValue);
+        }catch (InvalidOperatorException e){
+            // LogicalOperatorMapper threw an exception, so it's not a LogicalOperator.
+        }
+
+        try{
+            return this.conditionalOperatorMapper.getEntity(operatorValue);
+        }catch (InvalidOperatorException e){
+            // ConditionalOperatorMapper threw an exception, so it's not a ConditionalOperator.
+        }
+
+        throw new InvalidOperatorException(operatorValue);
+    }
+}
