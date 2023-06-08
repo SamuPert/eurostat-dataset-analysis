e3123b6ee0e294f24ea2de50fe43f37cc9422ab9
Samuele Perticarari <samuele.perticarari@gmail.com>
2023-06-08 15:53:28 +0200

    Created alias interface for Specification<MonetaryPoverty>.

---
+++ src/main/java/com/samupert/univpm/eurostat/filtering/criteria/SearchCriteriaSpecification.java
@@ -0,0 +1,7 @@
+package com.samupert.univpm.eurostat.filtering.criteria;
+
+import com.samupert.univpm.eurostat.monetary.poverty.MonetaryPoverty;
+import org.springframework.data.jpa.domain.Specification;
+
+public interface SearchCriteriaSpecification extends Specification<MonetaryPoverty> {
+}
