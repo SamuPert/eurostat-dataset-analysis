3feeeb45597efe4ddc953106c1905c411f80e8d4
Samuele Perticarari <samuele.perticarari@gmail.com>
2023-06-08 16:21:21 +0200

    Created EqualsNotConditionalSearchCriteria

---
+++ src/main/java/com/samupert/univpm/eurostat/filtering/criteria/conditional/EqualsNotConditionalSearchCriteria.java
@@ -0,0 +1,22 @@
+package com.samupert.univpm.eurostat.filtering.criteria.conditional;
+
+import com.samupert.univpm.eurostat.monetary.poverty.MonetaryPoverty;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+
+public class EqualsNotConditionalSearchCriteria extends EqualsConditionalSearchCriteria {
+
+    public EqualsNotConditionalSearchCriteria(String fieldName, Object value) {
+        super(fieldName, value);
+    }
+
+    @Override
+    protected Predicate getPredicate(Root<MonetaryPoverty> root,
+            CriteriaQuery<?> query,
+            CriteriaBuilder criteriaBuilder
+    ) {
+        return criteriaBuilder.not(super.getPredicate(root, query, criteriaBuilder));
+    }
+}
