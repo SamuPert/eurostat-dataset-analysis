fef36c51c585dc276bcd782b4b5b2f11c307121f
Samuele Perticarari <samuele.perticarari@gmail.com>
2023-06-08 12:23:59 +0200

    Created service method to handle specification filtering

--- src/main/java/com/samupert/univpm/eurostat/monetary/poverty/MonetaryPovertyService.java
+++ src/main/java/com/samupert/univpm/eurostat/monetary/poverty/MonetaryPovertyService.java
@@ -3,6 +3,7 @@ package com.samupert.univpm.eurostat.monetary.poverty;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,5 +14,9 @@ public class MonetaryPovertyService {
 
     public Page<MonetaryPoverty> getAll(Pageable page) {
         return this.repository.findAll(page);
+    }
+
+    public Page<MonetaryPoverty> searchByCriteria(Specification<MonetaryPoverty> specification, Pageable page) {
+        return this.repository.findAll(specification, page);
     }
 }
