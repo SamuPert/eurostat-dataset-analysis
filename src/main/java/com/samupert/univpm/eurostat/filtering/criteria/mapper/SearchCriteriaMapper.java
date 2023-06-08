eec0cde22a1bf5ec3a99b38a9543e789e3ff1139
Samuele Perticarari <samuele.perticarari@gmail.com>
2023-06-08 15:59:43 +0200

    Created interface for SearchCriteriaDto mappers.

---
+++ src/main/java/com/samupert/univpm/eurostat/filtering/criteria/mapper/SearchCriteriaMapper.java
@@ -0,0 +1,8 @@
+package com.samupert.univpm.eurostat.filtering.criteria.mapper;
+
+import com.samupert.univpm.eurostat.common.mapper.DtoToEntityMapper;
+import com.samupert.univpm.eurostat.filtering.SearchCriteriaDto;
+import com.samupert.univpm.eurostat.filtering.criteria.SearchCriteriaSpecification;
+
+public interface SearchCriteriaMapper extends DtoToEntityMapper<SearchCriteriaDto, SearchCriteriaSpecification> {
+}
