2984913a33f37621d0008fdc52853374d0210522
Samuele Perticarari <samuele.perticarari@gmail.com>
2023-06-08 16:21:44 +0200

    Created LessThanConditionalSearchCriteria

---
+++ src/main/java/com/samupert/univpm/eurostat/filtering/criteria/conditional/LessThanConditionalSearchCriteria.java
@@ -0,0 +1,31 @@
+package com.samupert.univpm.eurostat.filtering.criteria.conditional;
+
+import com.samupert.univpm.eurostat.monetary.poverty.MonetaryPoverty;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+
+import java.util.List;
+
+public class LessThanConditionalSearchCriteria extends ConditionalSearchCriteria<Number> {
+    public LessThanConditionalSearchCriteria(String fieldName, Object value) {
+        this.fieldName = fieldName;
+
+        // Assert that the value is a list of strings
+        this.value = ((Number) value);
+    }
+
+    @Override
+    protected Predicate getPredicate(Root<MonetaryPoverty> root,
+            CriteriaQuery<?> query,
+            CriteriaBuilder criteriaBuilder
+    ) {
+        return criteriaBuilder.lessThan(root.get(fieldName), Double.parseDouble(String.valueOf(value)));
+    }
+
+    @Override
+    protected List<Class<?>> getSupportedFieldTypes() {
+        return List.of(Number.class);
+    }
+}
