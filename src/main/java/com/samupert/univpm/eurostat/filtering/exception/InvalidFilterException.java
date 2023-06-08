7f28d7f88c8d12dd1af85290b06d91042e37cfca
Samuele Perticarari <samuele.perticarari@gmail.com>
2023-06-08 15:57:55 +0200

    Removed unused constructor.

--- src/main/java/com/samupert/univpm/eurostat/filtering/exception/InvalidFilterException.java
+++ src/main/java/com/samupert/univpm/eurostat/filtering/exception/InvalidFilterException.java
@@ -1,10 +1,6 @@
 package com.samupert.univpm.eurostat.filtering.exception;
 
 public class InvalidFilterException extends IllegalArgumentException {
-    public InvalidFilterException() {
-        super("Invalid filter. Check documentation for valid filters or check syntax.");
-    }
-
     public InvalidFilterException(String errorMessage) {
         super(errorMessage);
     }
