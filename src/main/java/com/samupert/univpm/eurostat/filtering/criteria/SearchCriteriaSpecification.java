168ea58d7474077f470af28875ade640410530f8
Samuele Perticarari <samuele.perticarari@gmail.com>
2023-06-16 19:06:20 +0200

    Documented SearchCriteriaSpecification

--- src/main/java/com/samupert/univpm/eurostat/filtering/criteria/SearchCriteriaSpecification.java
+++ src/main/java/com/samupert/univpm/eurostat/filtering/criteria/SearchCriteriaSpecification.java
@@ -3,5 +3,8 @@ package com.samupert.univpm.eurostat.filtering.criteria;
 import com.samupert.univpm.eurostat.monetary.poverty.MonetaryPoverty;
 import org.springframework.data.jpa.domain.Specification;
 
+/**
+ * Alias to Specification of MonetaryPoverty.
+ */
 public interface SearchCriteriaSpecification extends Specification<MonetaryPoverty> {
 }
