1bf0f597b2430062e9d5a916ce3b0523ef63c2f0
Samuele Perticarari <samuele.perticarari@gmail.com>
2023-06-16 19:05:46 +0200

    Documented MonetaryPovertyController

--- src/main/java/com/samupert/univpm/eurostat/monetary/poverty/controller/MonetaryPovertyController.java
+++ src/main/java/com/samupert/univpm/eurostat/monetary/poverty/controller/MonetaryPovertyController.java
@@ -15,6 +15,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Rest controller for the Monetary Poverty entity.
+ */
 @RestController
 @RequestMapping("/api/v1/monetary-poverty")
 @AllArgsConstructor
@@ -23,6 +26,13 @@ public class MonetaryPovertyController {
     private final MonetaryPovertyService monetaryPovertyService;
     private final CriteriaMapperFactory criteriaMapperFactory;
 
+    /**
+     * Get all Monetary Poverty data.
+     *
+     * @param pageNumber The page number to retrieve.
+     * @param pageSize The number of elements per page.
+     * @return The list of Monetary Poverty data for the given page.
+     */
     @GetMapping
     public List<MonetaryPoverty> getAll(
             @RequestParam(name = "page", defaultValue = "0") int pageNumber,
@@ -32,6 +42,14 @@ public class MonetaryPovertyController {
         return this.monetaryPovertyService.getAll(page).toList();
     }
 
+    /**
+     * Get all Monetary Poverty data filtered by a given search criteria.
+     *
+     * @param pageNumber The page number to retrieve.
+     * @param pageSize The number of elements per page.
+     * @param searchCriteria The search criteria to apply. See {@link SearchCriteriaDto}.
+     * @return The list of Monetary Poverty data for the given page, filtered by the provided search criteria.
+     */
     @PostMapping("search")
     public List<MonetaryPoverty> searchByCriteria(
             @RequestParam(name = "page", defaultValue = "0") int pageNumber,
@@ -46,6 +64,11 @@ public class MonetaryPovertyController {
         return this.monetaryPovertyService.searchByCriteria(searchCriteriaEntity, page).toList();
     }
 
+    /**
+     * Gets the metadata of the Monetary Poverty entity.
+     *
+     * @return The metadata of the Monetary Poverty entity. See {@link EntityMetadata}.
+     */
     @GetMapping("metadata")
     public ResponseEntity<EntityMetadata> getMetadata() {
         return ResponseEntity.ok(this.monetaryPovertyService.getMetadata());
