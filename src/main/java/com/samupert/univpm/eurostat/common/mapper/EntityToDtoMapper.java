696d5d59beabdac04f896d4234c4c43f5e520bae
Samuele Perticarari <samuele.perticarari@gmail.com>
2023-06-16 19:11:51 +0200

    Documented common mapper interfaces.

--- src/main/java/com/samupert/univpm/eurostat/common/mapper/EntityToDtoMapper.java
+++ src/main/java/com/samupert/univpm/eurostat/common/mapper/EntityToDtoMapper.java
@@ -1,5 +1,18 @@
 package com.samupert.univpm.eurostat.common.mapper;
 
+/**
+ * Interface that defines the contract for mapping between entities and DTOs.
+ *
+ * @param <D> The class of the DTO.
+ * @param <E> The class of the entity.
+ */
 public interface EntityToDtoMapper<D, E> {
+
+    /**
+     * Maps an entity to a DTO.
+     *
+     * @param entity The entity to be mapped.
+     * @return The DTO mapped from the entity.
+     */
     D getDto(E entity);
 }
