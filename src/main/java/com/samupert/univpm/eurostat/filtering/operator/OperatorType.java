faa4972f0e5bde8595ff22c620883c84ae6aab52
Samuele Perticarari <samuele.perticarari@gmail.com>
2023-06-16 19:08:09 +0200

    Documented operator type enum.

--- src/main/java/com/samupert/univpm/eurostat/filtering/operator/OperatorType.java
+++ src/main/java/com/samupert/univpm/eurostat/filtering/operator/OperatorType.java
@@ -1,5 +1,15 @@
 package com.samupert.univpm.eurostat.filtering.operator;
 
+/**
+ * Enumerates the types of operators that can be used in the filtering process.
+ */
 public enum OperatorType {
-    CONDITIONAL, LOGICAL
+    /**
+     * Identifies the {@link ConditionalOperator} type.
+     */
+    CONDITIONAL,
+    /**
+     * Identifies the {@link LogicalOperator} type.
+     */
+    LOGICAL
 }
