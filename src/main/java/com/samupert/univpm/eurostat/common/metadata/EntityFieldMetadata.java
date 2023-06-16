bf553cf234a3049950864dd7e405317551b8a9ae
Samuele Perticarari <samuele.perticarari@gmail.com>
2023-06-16 19:12:27 +0200

    Documented entity metadata classes.

--- src/main/java/com/samupert/univpm/eurostat/common/metadata/EntityFieldMetadata.java
+++ src/main/java/com/samupert/univpm/eurostat/common/metadata/EntityFieldMetadata.java
@@ -1,4 +1,10 @@
 package com.samupert.univpm.eurostat.common.metadata;
 
+/**
+ * Metadata of a field of an entity.
+ *
+ * @param name The name of the field.
+ * @param type The type of the field.
+ */
 public record EntityFieldMetadata(String name, String type) {
 }
--- src/main/java/com/samupert/univpm/eurostat/common/metadata/EntityMetadata.java
+++ src/main/java/com/samupert/univpm/eurostat/common/metadata/EntityMetadata.java
@@ -2,5 +2,11 @@ package com.samupert.univpm.eurostat.common.metadata;
 
 import java.util.List;
 
+/**
+ * Represents the metadata of an entity.
+ *
+ * @param entity the entity name.
+ * @param fields the list of fields of the entity. See {@link EntityFieldMetadata}.
+ */
 public record EntityMetadata(String entity, List<EntityFieldMetadata> fields) {
 }
