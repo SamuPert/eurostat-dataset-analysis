0753b75b2aa8097897005623da29986efd200d1a
Samuele Perticarari <samuele.perticarari@gmail.com>
2023-06-16 19:06:59 +0200

    Documented filtering exceptions.

--- src/main/java/com/samupert/univpm/eurostat/filtering/exception/InvalidFilterException.java
+++ src/main/java/com/samupert/univpm/eurostat/filtering/exception/InvalidFilterException.java
@@ -1,6 +1,14 @@
 package com.samupert.univpm.eurostat.filtering.exception;
 
+/**
+ * Exception thrown when the filter is not valid.
+ */
 public class InvalidFilterException extends IllegalArgumentException {
+    /**
+     * Create a new {@link InvalidFilterException}.
+     *
+     * @param errorMessage The error message.
+     */
     public InvalidFilterException(String errorMessage) {
         super(errorMessage);
     }
--- src/main/java/com/samupert/univpm/eurostat/filtering/exception/InvalidOperatorException.java
+++ src/main/java/com/samupert/univpm/eurostat/filtering/exception/InvalidOperatorException.java
@@ -1,6 +1,14 @@
 package com.samupert.univpm.eurostat.filtering.exception;
 
+/**
+ * Thrown when the operator is not valid.
+ */
 public class InvalidOperatorException extends InvalidFilterException {
+    /**
+     * Create a new {@link InvalidOperatorException}.
+     *
+     * @param operatorValue The operator value that is not valid.
+     */
     public InvalidOperatorException(String operatorValue) {
         super("Invalid operator: %s".formatted(operatorValue));
     }
