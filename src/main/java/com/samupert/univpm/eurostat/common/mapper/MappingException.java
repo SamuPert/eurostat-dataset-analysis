80858203c763eb97e3f00641e810ccd48c67d460
Samuele Perticarari <samuele.perticarari@gmail.com>
2023-06-16 19:12:37 +0200

    Documented common exceptions.

--- src/main/java/com/samupert/univpm/eurostat/common/mapper/MappingException.java
+++ src/main/java/com/samupert/univpm/eurostat/common/mapper/MappingException.java
@@ -1,6 +1,14 @@
 package com.samupert.univpm.eurostat.common.mapper;
 
+/**
+ * Exception thrown when a mapping fails.
+ */
 public class MappingException extends RuntimeException {
+    /**
+     * Creates a new {@link MappingException}.
+     *
+     * @param message The message to display
+     */
     public MappingException(String message) {
         super(message);
     }
